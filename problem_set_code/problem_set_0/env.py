0061f6c06cfe1b0eb62462efe7478fa48cc4e4a2
Drew-A-Hill <hill.dr@northeastern.edu>
2026-05-30 16:28:28 -0400

    Update to code

--- problem_set_code/problem_set_0/env.py
+++ problem_set_code/problem_set_0/env.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class ApartmentEnv(Env):
     
-    def __init__(self, T: int, K: int, seed=None):
+    def __init__(self, T: int, K: int, seed=None, noise_std: float=None):
         super().__init__()
         self.T = T
         self.K = K
@@ -13,10 +13,14 @@ class ApartmentEnv(Env):
         self.utils = None
         self.action_space = spaces.Discrete(2)
         self.observation_space = spaces.Box(low=np.array([1, 1]), high=np.array([T, K]), shape=(2, ), dtype=np.float32)
+        self.noise_std =noise_std
 
     def reset(self, seed=None, options=None) -> tuple:
         self.t = 1
         self.utils = np.random.randint(1, self.K + 1, size=self.T)
+
+        if self.noise_std is not None and self.noise_std > 0:
+            return (self.t, self.utils[self.t - 1] + self.noise_std) , {}
 
         return (self.t, self.utils[self.t - 1]), {}
 
@@ -35,5 +39,7 @@ class ApartmentEnv(Env):
         if self.t < self.T:
             self.t = self.t + 1
 
+        if self.noise_std is not None and self.noise_std > 0:
+            return (self.t, self.utils[u_t] + self.noise_std), reward, terminated, False, {}
 
         return (self.t, self.utils[u_t]), reward, terminated, False, {}
--- problem_set_code/problem_set_0/run.py
+++ problem_set_code/problem_set_0/run.py
@@ -16,12 +16,14 @@ table: dict = {
 random: list = []
 optimal: list = []
 
+std_noise: list = [0, 0.5, 1, 2]
+
 apt = env.ApartmentEnv(T, K)
 
 p_random: p.RandomPolicy = p.RandomPolicy(T)
 p_optimal: p.OptimalPolicy = p.OptimalPolicy(table)
 
-def run(policy, track):
+def run(policy, track, apt: env.ApartmentEnv):
     for i in range(N):
         obs = apt.reset()[0]
         terminated = False
@@ -45,7 +47,7 @@ def run(policy, track):
             if i == T - 1:
                 track.append(0)
 
-def run_t() -> np.array:
+def run_t(apt: env.ApartmentEnv) -> np.array:
     u_min: list = [1, 2, 3, 4]
     threshold: list = []
     max_mean: float = 0
@@ -75,6 +77,25 @@ def reject_all(rewards: np.array) -> float:
 
     return reject_all / N
 
+def run_noisy(noise) -> tuple:
+    apt_noisy = env.ApartmentEnv(T, K, noise)
+    r = []
+    o = []
+    run(p_random, r)
+    t_array: np.array = run_t()
+    run(p_optimal, o)
+
+    r_array: np.array = np.array(r)
+    o_array: np.array = np.array(o)
+
+    return r_array.mean(), t_array.mean(), o_array.mean()
+
+def all_noisy() -> None:
+    for i in range(len(std_noise)):
+        means: tuple = run_noisy(std_noise[i])
+
+        print(f"\nstd: {std_noise[i]}, Random Mean: {round(means[0], 3)}, Threshold Mean: {round(means[1], 3)}, Optimal Mean: {round(means[2], 3)}")
+
 run(p_random, random)
 run(p_optimal, optimal)
 
@@ -101,6 +122,8 @@ print(f"Optimal Policy Mean Reward: {round(o_mean, 3)}, Standard Error: {round(o
 print(f"The Random Policy rejected all apartments in {round(r_rej * 100, 2)}% of searches")
 print(f"The Threshold Policy rejected all apartments in {round(t_rej * 100, 2)}% of searches")
 print(f"The Optimal Policy rejected all apartments in {round(o_rej * 100, 5)}% of searches")
+
+all_noisy()
 
 plt.figure()
 plt.hist(r_array, label="Random Policy")
